bbc18d8865774ecee266660ab053a8db1f456944
Erika Mutzan <erikamtzmutzan@gmail.com>
2025-01-13 22:56:09 -0600

    style: colores y fuentes h1, h2 y p

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 function solicitarYCambiarNombre () {
     const nombre =prompt ("Cual es tu nombre?");
 // uso de la api DOM para cambiar el texto de un elemento
-document.getElementaryById("texto-saludo").innerText = "Hola" + nombre;
+document.getElementById("texto-saludo").innerText = "Hola " + nombre;
 
 }
 
